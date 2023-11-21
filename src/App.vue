708447f8fe749745ce323551f0f1d1938b60650c
federicomanni97 <manni_federico@libero.it>
2023-11-21 12:39:23 +0100

    no dynamic header nav setup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-  </div>
+  <body>
+    <div class="text-light">
+      <HeaderComponent/>
+    </div>
+    <h1>ciao</h1>
+  </body>
 </template>
 
+
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
   export default {
-    
+    components: {
+      HeaderComponent,
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+@use './assets/styles/partials/variables' as *;
 
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <nav class="navbar-expand-lg  navstyle">
+            <div class="collapse navbar-collapse container d-flex justify-content-between text-uppercase" id="navbarNav">
+                <img src="/public/images/logo.png" alt="Logo">
+                <div>
+                    <ul class="navbar-nav text-light">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features +</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Achievement</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Blog +</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item text-danger bg-light borderbutton1">
+                            <a class="nav-link disabled" href="#"><i class="fa-solid fa-user"></i> Login</a>
+                        </li>
+                        <li class="nav-item bgbutton borderbutton2">
+                            <a class="nav-link disabled" href="#"><i class="fa-solid fa-users"></i> Sign Up</a>
+                        </li> 
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+.navstyle{
+    background-color: var(--color-darkblue);
+}
+
+.bgbutton{
+    background-color: var(--color-lightred);
+}
+
+li{
+    padding: 0px 20px;
+}
+.borderbutton1{
+    border-bottom: 2px solid var(--color-lightred);
+}
+.borderbutton2{
+    border-right: 2px solid white;
+    border-bottom: 2px solid white;
+}
+</style>

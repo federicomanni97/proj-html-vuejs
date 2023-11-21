6b3c54305780571ce0223656329a0775c62e77d5
federicomanni97 <manni_federico@libero.it>
2023-11-21 21:05:59 +0100

    dynamic matches cards added

--- src/components/MainMatchesSchedule.vue
+++ src/components/MainMatchesSchedule.vue
@@ -5,7 +5,7 @@
             <h1 class="text-uppercase fw-bold py-3">All Matches Schedule</h1>
         </div>
         <div class="row align-items-center">
-            <div class="bglightred py-4 d-flex justify-content-between fs-4">
+            <div class="bglightred py-2 d-flex justify-content-between fs-4">
                 <div>
                     <span>2 GROUP 32 PLAYERS</span>
                 </div>
@@ -16,25 +16,61 @@
             <div class="col-12 d-flex align-items-center justify-content-between bg-white">    
                 <div class="text-black fs-6">
                     <h2 class="fs-3">Battlefield-4 Tournament</h2>
-                    <p>30 April 2022</p>
+                    <p class="text-secondary">30 April 2022</p>
                 </div>
                 <div class="mx-2 p-4">
-                    <img :src="store.smallLogos.lionLogo" alt="">
-                    <img class="vsimg" :src="store.smallLogos.vsLogo" alt="">
-                    <img :src="store.smallLogos.skullLogo" alt="">
+                    <img class="px-2" :src="store.smallLogos.lionLogo" alt="">
+                    <img class="vsimg smallwidth" :src="store.smallLogos.vsLogo" alt="">
+                    <img class="px-2" :src="store.smallLogos.skullLogo" alt="">
                 </div>
-                <div>
-                    <img class="px-2" :src="store.socialLogos.vimeoLogo" alt="">
-                    <img class="px-2" :src="store.socialLogos.youtubeLogo" alt="">
-                    <img class="px-2" :src="store.socialLogos.twitchLogo" alt="">
+                <div v-for="logos in store.socialLogos">
+                    <img class="smallwidth" :src="logos" alt="">
                 </div>
                 <div class="paddingt">
                     <button class="bglightred text-light text-uppercase border-0 p-3 fw-bold">Join Us Today <i class="fa-solid fa-circle-play"></i></button>
                 </div>
             </div>
         </div>
+        
+
         <div class="text-center py-5">
             <button class="boxshadow border-0 colorlightred text-uppercase p-3 fw-bold">Brows All Games <i class="fa-solid fa-circle-play"></i></button>
+        </div>
+        <div class="row">
+            <div class="col-6 g-5 align-items-center" v-for="logos in store.matchesLogos">
+                <div class="bglightred d-flex justify-content-between p-3 fs-4">
+                    <div>
+                        <span>2 GROUP 32 PLAYERS</span>
+                    </div>
+                    <div>
+                        <span>PRIZE POOL $3200</span>
+                    </div>
+                </div>
+                <div class="gx-5 d-flex align-items-center p-3 justify-content-between bg-white">    
+                    <div class="">
+                        <img class="" :src="logos.logo1" alt="">
+                    </div>
+                    <div class="">
+                        <div class="d-flex flex-column align-items-center">
+                            <div class="text-black fs-6">
+                                <h4 class="">Call of Duty Tournament</h4>
+                            </div>
+                            <div class="text-secondary d-flex">
+                                    <span>30 April 2022</span>
+                                    <span>Time: 08:30PM</span>
+                            </div>
+                            <div class="d-flex p-2">
+                                <div v-for="socialLogos in store.socialLogos">
+                                    <img class="p-1 smallwidth" :src="socialLogos" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <img class="" :src="logos.logo2" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -61,12 +97,22 @@ export default {
     background-color: var(--color-lightred);
 }
 
+img{
+    width: 90px;
+    height: 90px;
+}
+
 .mainsections{
     font-family: "Oswald", sans-serif;
 }
 .boxshadow{
     box-shadow: 3px 5px var(--color-lightred);
     border-style: none;
+}
+
+.smallwidth{
+    width: 50px;
+    height: 50px;
 }
 
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -13,26 +13,51 @@ export const store = reactive({
         thirdIcon: 'images/icon-3.png',
     },
     smallLogos: {
-        vsLogo: '/public/images/vs.png',
-        ninjaLogo: '/public/images/team-1.png',
-        vikingLogo: '/public/images/team-2.png',
-        dragonLogo: '/public/images/teamsm-1.png',
-        wolvesLogo: '/public/images/teamsm-2.png',
-        frogsLogo: '/public/images/teamsm-3.png',
-        monkeyhatLogo: '/public/images/teamsm-4.png',        
-        sharkLogo: '/public/images/teamsm-5.png',
-        lionLogo: '/public/images/teamsm-6.png',
-        robotLogo: '/public/images/teamsm-7.png',
-        witchLogo: '/public/images/teamsm-8.png',
-        skullLogo: '/public/images/teamsm-9.png',        
-        catLogo: '/public/images/teamsm-10.png',
-        redDragonLogo: '/public/images/teamsm-11.png',
-        monkeyLogo: '/public/images/teamsm-12.png',
+        vsLogo: '/images/vs.png',
+        ninjaLogo: '/images/team-1.png',
+        vikingLogo: '/images/team-2.png',
+        dragonLogo: '/images/teamsm-1.png',
+        wolvesLogo: '/images/teamsm-2.png',
+        frogsLogo: '/images/teamsm-3.png',
+        monkeyhatLogo: '/images/teamsm-4.png',        
+        sharkLogo: '/images/teamsm-5.png',
+        lionLogo: '/images/teamsm-6.png',
+        robotLogo: '/images/teamsm-7.png',
+        witchLogo: '/images/teamsm-8.png',
+        skullLogo: '/images/teamsm-9.png',        
+        catLogo: '/images/teamsm-10.png',
+        redDragonLogo: '/images/teamsm-11.png',
+        monkeyLogo: '/images/teamsm-12.png',
     },
-    socialLogos: {
-        vimeoLogo: '/public/images/social-1.png',
-        youtubeLogo: '/public/images/social-2.png',
-        twitchLogo: '/public/images/social-3.png',
-    }
+    socialLogos: [ 
+        '/images/social-1.png',
+        '/images/social-2.png',
+        '/images/social-3.png',
+    ],
+    matchesLogos: [
+        {
+            logo1: '/images/teamsm-1.png',
+            logo2: '/images/teamsm-2.png',
+        },
+        {
+            logo1: '/images/teamsm-7.png',
+            logo2: '/images/teamsm-8.png',
+        },
+        {
+            logo1: '/images/teamsm-3.png',
+            logo2: '/images/teamsm-4.png',
+        },
+        {
+            logo1: '/images/teamsm-9.png',
+            logo2: '/images/teamsm-10.png',
+        },
+        {
+            logo1: '/images/teamsm-5.png',
+            logo2: '/images/teamsm-6.png',
+        },
+        {
+            logo1: '/images/teamsm-11.png',
+            logo2: '/images/teamsm-12.png',
+        },
+    ]
 })
-
